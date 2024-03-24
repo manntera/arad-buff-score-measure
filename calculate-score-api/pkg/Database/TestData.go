28db9d42e91cbb9b1e619266a40450222176f50b
manntera <studio@manntera.com>
2024-03-24 11:25:47 +0900

    男クルセのテストデータを追加

--- calculate-score-api/pkg/Database/TestData.go
+++ calculate-score-api/pkg/Database/TestData.go
@@ -93,6 +93,164 @@ var TestDataList = []TestData{
 		},
 		Score: 1181373,
 	},
+	{
+		JobName: "encha",
+		ImageDataList: []ImageData{
+			{
+				ImageFileName: "test1.png",
+				SkillId:       8,
+			},
+			{
+				ImageFileName: "test2.png",
+				SkillId:       6,
+			},
+			{
+				ImageFileName: "test3.png",
+				SkillId:       7,
+			},
+			{
+				ImageFileName: "test4.png",
+				SkillId:       9,
+			},
+			{
+				ImageFileName: "test5.png",
+				SkillId:       10,
+			},
+		},
+		SuccessParamList: []BuffSkillParam{
+			{
+				SkillId: 8,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 532,
+					},
+				},
+			},
+			{
+				SkillId: 6,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 19846,
+					},
+					{
+						ParamId:    2,
+						ParamValue: 3823,
+					},
+				},
+			},
+			{
+				SkillId: 7,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 40710,
+					},
+				},
+			},
+			{
+				SkillId: 9,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 4961,
+					},
+					{
+						ParamId:    2,
+						ParamValue: 955,
+					},
+				},
+			},
+			{
+				SkillId: 10,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 10991,
+					},
+				},
+			},
+		},
+		Score: 274212,
+	},
+	{
+		JobName: "mkuruse",
+		ImageDataList: []ImageData{
+			{
+				ImageFileName: "test1.png",
+				SkillId:       19,
+			},
+			{
+				ImageFileName: "test2.png",
+				SkillId:       17,
+			},
+			{
+				ImageFileName: "test3.png",
+				SkillId:       18,
+			},
+			{
+				ImageFileName: "test4.png",
+				SkillId:       20,
+			},
+			{
+				ImageFileName: "test5.png",
+				SkillId:       21,
+			},
+		},
+		SuccessParamList: []BuffSkillParam{
+			{
+				SkillId: 19,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 397,
+					},
+				},
+			},
+			{
+				SkillId: 17,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 45930,
+					},
+					{
+						ParamId:    2,
+						ParamValue: 8239,
+					},
+				},
+			},
+			{
+				SkillId: 18,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 84231,
+					},
+				},
+			},
+			{
+				SkillId: 20,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 53,
+					},
+				},
+			},
+			{
+				SkillId: 21,
+				BuffParams: []BuffParam{
+					{
+						ParamId:    1,
+						ParamValue: 24426,
+					},
+				},
+			},
+		},
+		Score: 745080,
+	},
 }
 
 func GetSuccessParamFromSkillId(skillId int) *BuffSkillParam {
