a551cd9239d88f9fc76199fc7dbe58aaaf5e04d8
mtakuma1110 <mtakuma1110@gmail.com>
2024-04-16 21:23:19 +0900

    API呼び出し検証中

--- client-python/apicallsample.py
+++ client-python/apicallsample.py
@@ -1,60 +1,137 @@
 from google.oauth2 import service_account
+from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request, AuthorizedSession
 from requests_toolbelt.multipart.encoder import MultipartEncoder
+from google.auth.exceptions import RefreshError
 import requests
 import sys
 import json
 
 
-def getIDTokenCredentials():
-    # サービスアカウントの鍵ファイルパス
-    key_path = "calculate-score-api-credential.json"
+# def callapi(files):
+    
+#     # APIエンドポイント
+#     CLOUD_RUN_URL = 'https://arad-buff-score-measure.manntera.com/calculate-score'
 
-    # サービスアカウント認証情報のロード
+#     # サービスアカウントの鍵ファイルのパス
+#     SERVICE_ACCOUNT_FILE = 'calculate-score-api-credential.json'
+
+#     # サービスアカウントキーを使用して認証情報を取得
+#     credentials = service_account.Credentials.from_service_account_file(
+#         SERVICE_ACCOUNT_FILE,
+#         # Cloud Run APIを呼び出すために必要なスコープ
+#         scopes=['https://www.googleapis.com/auth/cloud-platform'],
+#     )
+
+#     # 認証情報をリフレッシュしてアクセストークンを取得
+#     request = Request()
+#     credentials.refresh(request)
+#     print("token: ", credentials.token)
+
+#     # アクセストークンを返す
+#     return credentials.token
+
+#     # # 認証済みセッションを作成
+#     # authed_session = requests.Session()
+#     # auth_request = Request(session=authed_session)
+
+#     # # 認証情報をセッションに適用
+#     # credentials.refresh(auth_request)
+
+#     # authed_session.headers.update({
+#     #     'Authorization': f'Bearer {credentials.token}'
+#     # })
+
+#     # print("token: ", credentials.token)
+
+#     # # Create an authorized session that automatically handles token refresh
+#     # authed_session = AuthorizedSession(credentials)
+
+#     # # 認証済みセッションを使用してPOSTリクエストを送信
+#     # response = authed_session.post(CLOUD_RUN_URL, files=files)
+
+#     response = None
+#     return response
+
+def callapi2(files):
+    # サービスアカウントキーファイルへのパス
+    KEY_PATH = "calculate-score-api-credential.json"
+
+    # Cloud Run サービスの URL
+    API_URL = "https://arad-buff-score-measure.manntera.com/calculate-score"
+
+    # サービスアカウントの認証情報をロード
     credentials = service_account.IDTokenCredentials.from_service_account_file(
-        key_path,
+        KEY_PATH,
         target_audience='https://arad-buff-score-measure.manntera.com/calculate-score'
     )
 
-    # 認証情報を使用してIDトークンを取得するためのリクエストを行う
+    # 認証情報をリフレッシュしてIdentity Tokenを取得
     request = Request()
     credentials.refresh(request)
 
-    return credentials
+    # Identity Tokenの表示
+    print("Identity Token:", credentials.token)
 
-def callapi(files):
+    # アクセストークンを使用して API リクエスト
+    headers = {
+        'Authorization': f'Bearer {credentials.token}',
+        'Content-Type': 'application/json'
+    }
+
+    # POST リクエスト
+    response = requests.post(API_URL, headers=headers, files=files)
+
+    # レスポンスの表示
+    print(response.status_code)
+    print(response.text)
     
-    # APIエンドポイント
-    CLOUD_RUN_URL = 'https://arad-buff-score-measure.manntera.com/calculate-score'
+# def print_access_token():
+#     # 認証情報ファイルのパスを指定
+#     credentials_path = 'calculate-score-api-credential.json'
 
-    # サービスアカウントの鍵ファイルのパス
-    SERVICE_ACCOUNT_FILE = 'calculate-score-api-credential.json'
+#     # 認証情報をロード
+#     creds = Credentials.from_authorized_user_file(credentials_path)
 
-    # サービスアカウントキーを使用して認証情報を取得
-    credentials = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE,
-        # Cloud Run APIを呼び出すために必要なスコープ
-        scopes=['https://www.googleapis.com/auth/cloud-platform'],
-    )
+#     # トークンが有効か、またはリフレッシュ可能か確認
+#     if not creds.valid:
+#         if creds.expired and creds.refresh_token:
+#             try:
+#                 creds.refresh(Request())
+#             except RefreshError as e:
+#                 print("Failed to refresh access token:", str(e))
+#                 return
+#         else:
+#             print("No valid refresh token is available.")
+#             return
 
-    # 認証済みセッションを作成
-    authed_session = AuthorizedSession(credentials)
+#     # アクセストークンを出力
+#     print("Access Token:", creds.token)
 
-    # 認証済みセッションを使用してPOSTリクエストを送信
-    response = authed_session.post(CLOUD_RUN_URL)
-
-    return response
-
-    
 # 複数の画像ファイルをフォームデータとして準備
-files = [
-   ('images', open('test_image/test1.png', 'rb')),
-   ('images', open('test_image/test2.png', 'rb')),
+# files = [
+#    ('images', open('test_image/test1.png', 'rb')),
+#    ('images', open('test_image/test2.png', 'rb')),
 #    ('images', open('test_image/test3.png', 'rb')),
 #    ('images', open('test_image/test4.png', 'rb')),
-#    ('images', open('test_image/test5.png', 'rb')),
-#    ('images', open('test_image/test6.png', 'rb'))
-]
+#    ('images', open('test_image/test5.png', 'rb'))
+#  ]
+
+# file_paths = [
+#     'test_image/test1.png', 
+#     'test_image/test2.png', 
+#     'test_image/test3.png'
+# ]
+# files = {'images': [(open(file_path, 'rb')) for file_path in file_paths]}
+
+# 画像ファイルのリスト
+# files = {
+#     'images': [('test1.png', open('test_image/test1.png', 'rb')),
+#                 ('test2.png', open('test_image/test2.png', 'rb')),
+#                 ('test3.png', open('test_image/test3.png', 'rb')),
+#                 ('test4.png', open('test_image/test4.png', 'rb')),
+#                 ('test5.png', open('test_image/test5.png', 'rb'))]
+# }
 
 # マルチパートエンコーダーの作成
 # files = MultipartEncoder(
@@ -69,12 +146,30 @@ files = [
 #     }
 # )
 
-# POSTリクエストを送信
-response = callapi(files)
+files = {
+    'file': ('filename', open('test_image/test1.png', 'rb'))
+}
 
-# レスポンスを表示
-print(response.text)
+try:
+    # POSTリクエストを送信
+    response = callapi2(files)
 
-# 使用後にファイルを閉じる
-for _, file in files:
-    file.close()
+finally:
+    # 使用後にファイルを閉じる
+    # for file in files['images']:
+    #     file[1].close()
+    print("end")
+
+
+# with open(file_paths[0], 'rb') as file1, open(file_paths[1], 'rb') as file2, open(file_paths[2], 'rb') as file3:
+#     files = {
+#         'image1': ('image1.png', file1),
+#         'image2': ('image2.png', file2),
+#         'image3': ('image3.png', file3)
+#     }
+
+#     # Send the POST request using the authenticated session
+#     response = callapi(files)
+#     print(response.status_code)
+#     print(response.text)
+
